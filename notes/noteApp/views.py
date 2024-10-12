6b68c4229945fbba6c10de207581b778d01b340e
Sourabh Bundele <sbundele0318@gmail.com>
2024-10-12 16:36:48 +0530

    created a view and make some changes to model, added a note using admin interface

--- notes/noteApp/views.py
+++ notes/noteApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+@api_view(["GET", "POST"])
+def notes(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializers = NoteSerializer(notes, many=True)
+        return Response(serializers.data)
+    elif request.method == 'POST':
+        serializers = NoteSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
